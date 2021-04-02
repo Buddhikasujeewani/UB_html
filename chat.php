0f62a8c85ffd5ea67b77fc941b8251e101532a38
Buddhikasujeewani <72122926+Buddhikasujeewani@users.noreply.github.com>
2021-04-02 15:12:28 +0530

    Initial commit

---
+++ chat.php
@@ -0,0 +1,28 @@
+<div class=""style="background: #3b1713;
+    color: white;
+    padding: 10px;
+    font-weight: bold;
+    font-size: 18px;
+    margin-top: 0px;
+   text-align:left;
+    font-family: Helvetica Neue,Helvetica,Arial,sans-serif;">
+  #Public Chatroom
+</div>
+<div id="wf-chat-frame" class="row no-gutters">
+  <div class="col-12">
+    <div id="wf-chat-messages">
+      <div id="wf-messages-container">
+        <div id="wf-chat-messages-inner">
+            
+          <ul id="000000-000000" class="messages-content"></ul>
+        </div>
+      </div>
+      <div class="message-input">
+        <div class="wrap">
+          <input type="text" id="message-input-text" placeholder="Write your message..." />
+          <button class="submit" id="message-submit">Send</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ poll.php
@@ -0,0 +1,16 @@
+<h3 style="font-family:Nunito Sans;">Do you think schools should remain open during the upcoming lockdown?</h3>
+<div id="wf-poll-form" <?php if (!empty($answer)) {echo 'style="display:none;"';}?>>
+  <ul>
+    <li class="wf-poll-option" id="wf-option-y" data-answer="Y" data-uid="<?php echo $uid; ?>">Yes</li>
+    <li class="wf-poll-option" id="wf-option-n" data-answer="N" data-uid="<?php echo $uid; ?>">No</li>
+    <li class="wf-poll-option" id="wf-option-x" data-answer="X" data-uid="<?php echo $uid; ?>">Not Sure</li>
+  </ul>
+</div>
+<div id="wf-poll-results" <?php if (empty($answer)) {echo 'style="display:none;"';}?>>
+  <div class="wf-waiting">
+    <h4><b>Updating poll results...</b></h4>
+  </div>
+  <canvas id="wf-poll-graph" width="100%"></canvas>
+  <a href="https://wildfirecomms.typeform.com/to/N0Ir1i7x" target="_blank" id="wf-poll-submit"
+    class="btn float-right">TAKE THE SURVEY</a>
+</div>
